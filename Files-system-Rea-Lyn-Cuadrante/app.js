e7c064859620245ddba6293a7fc656be91ac0c8d
Rea Lyn G. Cuadrante <cuadranterealyn68@gmail.com>
2022-10-16 21:00:48 +0800

    Add files via upload

---
+++ Files-system-Rea-Lyn-Cuadrante/app.js
@@ -0,0 +1,9 @@
+const cmd = process.argv[2];
+
+const write = require('./write');
+
+write(cmd);
+
+const read = require('./read');
+
+console.log(read());
---
+++ Files-system-Rea-Lyn-Cuadrante/read.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const read = function() {
+     return fs.readFileSync('./task.txt' ,
+     {
+        encoding: 'utf8',
+        flag: 'r'
+    }
+    )
+}
+
+module.exports = read;
